41bf9cad89211ac57a7279cd5e5fcfff4f893875
Abdul Basit <bitm-f19-046@superior.edu.pk>
2022-02-13 20:46:22 +0500

    Assignment 1

---
+++ app/src/main/java/com/example/kfc/login.java
@@ -0,0 +1,35 @@
+package com.example.kfc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openregister();
+
+
+            }
+        });
+    }
+
+    public void openregister(){
+
+        Intent intent = new Intent(this,register.class);
+        startActivity(intent);
+
+
+    }
+}
